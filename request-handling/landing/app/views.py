352f549f9512123026c4f5ef841ac0a5fe2497ba
feelsever <philipp.bhandari@gmail.com>
2019-07-30 21:28:24 +0300

    first done

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -12,6 +12,11 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    from_landing = request.GET.get('from-landing')
+    if from_landing == 'original':
+        counter_click['original'] += 1
+    elif from_landing == 'test':
+        counter_click['test'] += 1
     return render_to_response('index.html')
 
 
@@ -20,6 +25,11 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
+
+    if request.GET.get('ab-test-arg') == 'test':
+        counter_show['test'] += 1
+        return render_to_response('landing_alternate.html')
+    counter_show['original'] += 1
     return render_to_response('landing.html')
 
 
@@ -28,7 +38,16 @@ def stats(request):
     # Чтобы отличить с какой версии лендинга был переход
     # проверяйте GET параметр marker который может принимать значения test и original
     # Для вывода результат передайте в следующем формате:
+    try:
+        test_conversion = counter_click['test'] / counter_show['test']
+    except ZeroDivisionError:
+        test_conversion = 0
+    try:
+        original_conversion = counter_click['original'] / counter_show['original']
+    except ZeroDivisionError:
+        original_conversion = 0
+
     return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': round(test_conversion, 2),
+        'original_conversion': round(original_conversion, 2),
     })
