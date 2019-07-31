d562970dab3bf6ffbacadd46f033254c0af1e2d8
feelsever <philipp.bhandari@gmail.com>
2019-07-31 15:48:26 +0300

    second done

--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -1,29 +1,70 @@
 import datetime
+import os
 
 from django.shortcuts import render
+from app.settings import FILES_PATH
 
 
-def file_list(request):
+def file_list(request, date=None):
     template_name = 'index.html'
-    
-    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
     context = {
-        'files': [
-            {'name': 'file_name_1.txt',
-             'ctime': datetime.datetime(2018, 1, 1),
-             'mtime': datetime.datetime(2018, 1, 2)}
-        ],
-        'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
+        'files': []
     }
+    files_list = os.listdir(FILES_PATH)
+
+    if date:
+        date = date.strip()
+        try:
+            new_date = datetime.datetime.strptime(date, '%Y-%m-%d')
+            context['date'] = str(new_date.date())
+        except Exception as e:  # Вот тут всё время вылетает ошибка, не пойму почему
+            print(e)
+
+    for file in files_list:
+        c = round(os.stat(os.path.join(FILES_PATH, file)).st_ctime)
+        m = round(os.stat(os.path.join(FILES_PATH, file)).st_mtime)
+
+        try:
+            if context['date'] != str(datetime.datetime.fromtimestamp(c).date()):
+                continue
+        except KeyError:
+            pass
+
+        context['files'].append({
+            'name': file,
+            'ctime': {
+                'date': str(datetime.datetime.fromtimestamp(c).date()),
+                'time': str(datetime.datetime.fromtimestamp(c).time())
+            },
+            'mtime': {
+                'date': str(datetime.datetime.fromtimestamp(m).date()),
+                'time': str(datetime.datetime.fromtimestamp(m).time())
+            },
+        })
 
     return render(request, template_name, context)
 
 
 def file_content(request, name):
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    return render(
-        request,
-        'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
-    )
+    try:
+        file = os.path.join(FILES_PATH, name)
+        with open(file) as f:
+            s = f.read()
+            return render(request, 'file_content.html', context={
+                    'file_name': name,
+                    'file_content': s
+                }
+            )
+    except FileNotFoundError:
+        return render(request, 'file_content.html', context={
+                'file_name': 'Такого файла не существует',
+                'file_content': 'Сорямба!'
+            }
+        )
+
+
+
+
+
 
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/feelsever/PycharmProjects/DJANGO_HOMEWORKS/dj-homeworks/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
