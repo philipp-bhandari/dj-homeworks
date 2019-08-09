00b82ea37d5af96189efccb5b73acc0ed446fadf
feelsever <philipp.bhandari@gmail.com>
2019-08-09 15:50:22 +0300

    first done

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,47 @@
 from django.shortcuts import render
+import csv
+import os
+
 
 def inflation_view(request):
     template_name = 'inflation.html'
+    context = {
+        'data': []
+    }
+    with open(os.path.abspath('inflation_russia.csv'), newline='') as csv_file:
+        reader = csv.reader(csv_file, delimiter=';')
 
-    # чтение csv-файла и заполнение контекста
-    context = {}
+        for i, row in enumerate(reader):
+            data_list = []
+            for k, td in enumerate(row):
+                if row[k] is row[-1]:
+                    color = 'gray'
+                    num = td
+                elif row[k] is row[0]:
+                    color = None
+                    num = td
+                else:
+                    try:
+                        num = float(td)
+                        if num < 0:
+                            color = 'green'
+                        elif 1 <= num < 2:
+                            color = 'lightsalmon'
+                        elif 2 <= num < 5:
+                            color = 'salmon'
+                        elif num >= 5:
+                            color = 'indianred'
+                        else:
+                            color = None
+                    except:
+                        num = td
+
+                data = {
+                    'color': color,
+                    'data': num
+                }
+                data_list.append(data)
+            context['data'].append(data_list)
 
     return render(request, template_name,
-                  context)
+                  context=context)
